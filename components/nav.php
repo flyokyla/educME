52895c319330c8fd504d666f9e68494eb74454d1
flyokyla <akapytin@gmail.com>
2024-09-08 22:06:58 +0300

    header на php + небольшие изменение

---
+++ components/nav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru" >
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="nav.css" />
+    <title>ТЕСТ</title>
+</head>
+<body style="margin: 0%;">
+    <nav class="navig-site">
+        <div class="navig-site-in">
+        <a class="logo-link" href="../index.html"><img class="logo" src="../images/photo.jpg"  title="главная" alt="главная"/></a>
+        <ul class="navig-list">
+            <li><a href="HTMLstruct/struct.html"> HTML </a></li>
+            <li> <a href="HTMLstruct/razmetka/index.html">БЛОКИ HTML</a> </li>
+            <li> <a href="HTMLstruct/registr/registr.html" target="_blank"> ФОРМЫ HTML </a> </li>
+            <li> <a href="CSSstruct/csstruct.html"> CSS </a> </li>
+            <li> <a href="lessPHP/structPHP.html"> PHP </a> </li>
+            <li> <a href="phishki/phishki.html"> ФИШКИ</a> </li>
+            <li> <a href="GITstruct/gitt.html">GIT</a> </li>
+        </ul>
+        </div>
+    </nav>
+</body>
+</html>
